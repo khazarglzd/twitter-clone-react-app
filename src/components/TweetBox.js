aa63e4e4ddea7d3f77b9f207ca9e3d095dc2bf9e
khazarglzd <101716414+khazarglzd@users.noreply.github.com>
2022-04-23 01:44:11 +0400

    creating content area components

---
+++ src/components/TweetBox.js
@@ -0,0 +1,43 @@
+import React from "react";
+import {
+  EmojiIcon,
+  GIFIcon,
+  ImageIcon,
+  PollIcon,
+  ScheduleIcon,
+} from "../icons/Icon";
+
+const TweetBox = () => {
+  return (
+    <div className="flex flex-col flex-1 mt-2 px-2">
+      <textarea
+        className="w-full text-xl placeholder-gray-dark outline-none overflow-hidden resize-none bg-transparent"
+        placeholder="What's happening?"
+      />
+      <div className="flex items-center justify-between">
+        <div className="flex -ml-3">
+          <div className="flex items-center justify-center w-11 h-11 rounded-full hover:bg-gray-lightest">
+            <ImageIcon className="w-6 h-6 text-primary-base" />
+          </div>
+          <div className="flex items-center justify-center w-11 h-11 rounded-full hover:bg-gray-lightest">
+            <GIFIcon className="w-6 h-6 text-primary-base" />
+          </div>
+          <div className="flex items-center justify-center w-11 h-11 rounded-full hover:bg-gray-lightest">
+            <PollIcon className="w-6 h-6 text-primary-base" />
+          </div>
+          <div className="flex items-center justify-center w-11 h-11 rounded-full hover:bg-gray-lightest">
+            <EmojiIcon className="w-6 h-6 text-primary-base" />
+          </div>
+          <div className="flex items-center justify-center w-11 h-11 rounded-full hover:bg-gray-lightest">
+            <ScheduleIcon className="w-6 h-6 text-primary-base" />
+          </div>
+        </div>
+        <button className="bg-primary-base text-white rounded-full px-4 py-2 font-medium">
+          Tweet
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default TweetBox;
--- src/layout/Content.js
+++ src/layout/Content.js
@@ -1,9 +1,24 @@
 import React from "react";
+import Divider from "../components/Divider";
+import TweetBox from "../components/TweetBox";
+import { PopulerIcon } from "../icons/Icon";
 
 const Content = () => {
   return (
-    <main className="flex-1 flex flex-col border-r border-l bg-slate-300 border-gray-extraLight">
-      <h1>Content</h1>
+    <main className="flex-1 flex flex-col border-r border-l border-gray-extraLight">
+      <header className="sticky top-0 z-10 bg-white flex justify-between items-center p-4 border-b border-gray-extraLight ">
+        <span className="font-bold text-xl text-gray-900">Home</span>
+        <PopulerIcon className="w-6 h-6 text-primary-base" />
+      </header>
+      <div className="flex space-x-4 px-4 py-3">
+        <img
+          src="https://avatars.githubusercontent.com/u/101716414?v=4"
+          alt="Profile"
+          className="w-11 h-11 rounded-full"
+        />
+        <TweetBox />
+      </div>
+      <Divider />
     </main>
   );
 };
